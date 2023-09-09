7cc8f37e83201b8ddd6dc3472636359e5b1d72e5
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2023-09-09 02:58:44 +0000

    add header, comment and about component. Building comment card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommentComponent } from '../components/comment/comment.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CommentComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
