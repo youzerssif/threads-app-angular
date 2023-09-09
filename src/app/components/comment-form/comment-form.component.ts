4f0d9bcd8d394fe3cc204f258924dfc37225469e
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2023-09-09 04:15:39 +0000

    Finish to Build comment card, lets go for create service

---
+++ src/app/components/comment-form/comment-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-comment-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './comment-form.component.html',
+  styleUrls: ['./comment-form.component.scss']
+})
+export class CommentFormComponent {
+
+}
--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommentFormComponent } from '../comment-form/comment-form.component';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CommentFormComponent],
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent {
+
+  IsExpanded = false;
+
+  IsReplying = false;
+
+
+  toggleExpanded(){
+    this.IsExpanded =!this.IsExpanded;
+  }
+
+  toggleReplying(){
+    this.IsReplying =!this.IsReplying;
+    if(this.IsReplying){
+      this.IsExpanded = true;
+    }
+  }
 
 }
